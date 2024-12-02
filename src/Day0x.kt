b6d588518f6e3b9d88a6c540c5f764d933c2b30e
Ajith kumar SR <ajithkumarsr@Ajiths-MacBook-Pro.local>
2024-12-02 14:09:27 +0530

    creating a template file

--- src/Day0x.kt
+++ src/Day0x.kt
@@ -1,42 +1,18 @@
-import kotlin.math.absoluteValue
-
 fun main() {
     fun part1(input: List<String>): Int {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-
-        input.map { it.split(" ") }
-            .forEach { list1.add(it.first().toInt()); list2.add(it.last().toInt()); }
-        list1.sort()
-        list2.sort()
-
-        val result = mutableListOf<Int>()
-        list1.forEachIndexed { index, _ -> val distance = (list1[index] - list2[index]).absoluteValue; result.add(distance) }
-        return result.sum()
+        return input.size
     }
 
     fun part2(input: List<String>): Int {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-
-        input.map { it.split(" ") }
-            .forEach { list1.add(it.first().toInt()); list2.add(it.last().toInt()); }
-        val countMap = mutableMapOf<Int, Int>()
-        list1.forEach { countMap[it] = 0 }
-        list2.forEach { countMap[it] = countMap[it]?.plus(1) ?: 1 }
-
-        val result = mutableListOf<Int>()
-        list1.forEachIndexed { index, _ -> val sscore = (list1[index] * countMap[list1[index]]!!); result.add(sscore) }
-        return result.sum()
+        return input.size
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("3 4","4 3","2 5","1 3","3 9","3 3")) == 11)
-    check(part2(listOf("3 4","4 3","2 5","1 3","3 9","3 3")) == 31)
+    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01.txt` file:
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 1000)
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
